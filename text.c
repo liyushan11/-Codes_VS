4fac88d3a3bb89e2be8aa33b4a88854ba8258bb5
537li <2543347978@qq.com>
2026-03-10 19:50:03 +0800

    third commit

--- text.c
+++ text.c
@@ -2,6 +2,7 @@
 int main()
 {
     printf("hello world!");
-    printf("this is second commit,just test the git!");
+    printf("this is second commit,just test the git!\n");
+    printf("This is the third change,just test the continue commit!\n");
     return 0;
 }
